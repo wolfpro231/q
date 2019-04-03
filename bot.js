8227ca48594fb020921b899ad0b30b8dfa208e9d
wolfpro231 <48035876+wolfpro231@users.noreply.github.com>
2019-04-03 19:49:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
-var adminprefix = '#'
+var adminprefix = 'r'
 
 
-const developers = ["ID","ID"]
+const developers = ["513095059579863092"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
